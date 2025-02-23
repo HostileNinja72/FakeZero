33c63152e5e29b27736e223dfa34b4d06c76d30f
HostileNinja72 <hostileninja72@proton.me>
2025-02-23 14:46:52 +0000

    Initial commit

---
+++ fake-news-frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Analyzer from "./components/Analyzer";
+
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Analyzer />
+           
+        </div>
+    );
+};
+
+export default App;
