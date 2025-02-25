298ecf2eb8018eee3c1a9787f31354345614a9d1
HostileNinja72 <hostileninja72@proton.me>
2025-02-25 01:32:34 +0000

    modifs

--- react/frontend/components/Front.jsx
+++ react/frontend/components/Front.jsx
@@ -9,12 +9,12 @@ export default function Front()
     return (
    <div className="full">
     <div className="title">
-        <div>Detect Fake News Instantly with <span>AI</span>  </div>
+        <div className="inter">Detect Fake News Instantly with <span>AI</span>  </div>
         <div className="p">Our powerful AI-driven Fake News Detector helps you verify the credibility of news articles, social media posts, and online content in seconds.</div>
         <div className="create">Create Free Account</div>
     </div>
     <div className="enter">
-        <input placeholder="Past your text"></input>
+        <input className="ph" placeholder="Paste your text"></input>
         <div className="last">
 
         <div className="verify"><div>Verify</div>
